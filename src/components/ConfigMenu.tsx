01a005b980ee93ceb1403ecdd3432301d94e2b8f
Alejandro León <juan.aleon2001@gmail.com>
2023-04-18 00:51:52 -0500

    new: form to set name

--- src/components/ConfigMenu.tsx
+++ src/components/ConfigMenu.tsx
@@ -1,17 +1,37 @@
 import '../css/ConfigMenu.css';
+import React, { useState } from 'react';
 
 type ConfigMenuProps = {
   isOpen: boolean;
 };
 
 const ConfigMenu = ({ isOpen }: ConfigMenuProps) => {
+  // Declaración de la variable de estado "nombre" y su función "setNombre"
+
+  const [nombre, setNombre] = useState('');
+
+  // Manejador del evento "onChange" para actualizar el estado "nombre"
+  const handleChange = (event: any) => {
+    setNombre(event.target.value);
+  };
+
+  // Manejador del evento "onSubmit" para el formulario
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    // Aquí puedes realizar acciones con el nombre, como enviarlo a un servidor o mostrar un mensaje
+    alert('Nombre ingresado: ' + nombre);
+  };
+
   return (
     <>
-      <div className={'config-menu ' + (isOpen ? 'mostrar' : 'ocultar') }>
+      <div className={'config-menu ' + (isOpen ? 'mostrar' : 'ocultar')}>
         <h2>Configuración</h2>
         <div className='user-name'>
-          <label htmlFor='name'>Nombre: </label>
-          <input type='text' name='name' id='name' />
+          <form action='' id='form-name' onSubmit={handleSubmit}>
+            <label htmlFor='name'>Nombre: </label>
+            <input type='text' name='name' value={nombre} id='name' onChange={handleChange}/>
+            <input type='submit' value={'Establecer nombre'} />
+          </form>
         </div>
       </div>
     </>
