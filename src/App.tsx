e8dc73797927de84e26aee054015380619356531
Alejandro León <juan.aleon2001@gmail.com>
2023-04-30 16:06:36 -0500

    new: updated project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { createContext, useState } from 'react';
 import Navbar from './components/Navbar';
 import Plano from './components/Plano';
 import './App.css';
-import ConfigMenu from './components/ConfigMenu';
-import Context from './components/Context';
 import SpeechRecognition from './components/SpeechRecognition';
 
 function App() {
-  const [openModal, setOpenModal] = useState(false);
-
   return (
     <div className='container'>
-      <Context.Provider value={openModal}>
-        <Navbar setOpenModal={setOpenModal} />
-        <Plano />
-      </Context.Provider>
-      <ConfigMenu isOpen={openModal} />
+      <Navbar />
+      <Plano />
       <SpeechRecognition />
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,12 @@
 import { useContext } from 'react';
 import '../css/Navbar.css';
-import Context from './Context';
 
 const Navbar = ({ setOpenModal }: any) => {
-  const openModal = useContext(Context);
   return (
     <nav className='navbar'>
       <h3 className='navbar-title'>Smart House</h3>
       <div className='settings'>
-        <i
-          className='fa-solid fa-gear'
-          onClick={() => setOpenModal(!openModal)}
-        ></i>
+        <i className='fa-solid fa-gear'></i>
       </div>
     </nav>
   );
