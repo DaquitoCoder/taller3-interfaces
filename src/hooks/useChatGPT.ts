40db0932db131955aa01c01cbd0b19675fb4361d
Alejandro León <juan.aleon2001@gmail.com>
2023-05-30 00:21:06 -0500

    new: added username to context message

--- src/hooks/useChatGPT.ts
+++ src/hooks/useChatGPT.ts
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
 import { MessageProps } from '../components/chat/Message';
+import { useStatesStore } from '../store/StatesStore';
 
 type GPTMessage = {
   role: 'user' | 'assistant' | 'system';
   content: string;
 };
 
-const firstMessage: GPTMessage = {
-  role: 'system',
-  content:
-    'You are an house automative assistant, your name is HomeGenius. You have to help to the user to control the house. You can search things in your storage and you can control the lights and the temperature. You can also help to the user to find things in the house.',
-};
-
 export function useChatGPT() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const apiKey = import.meta.env.VITE_APP_OPENAI_API_KEY;
+  const { username } = useStatesStore();
+
+  const firstMessage: GPTMessage = {
+    role: 'system',
+    content: `You are an house automative assistant, your name is HomeGenius. You have to help to the user to control the house. You can search things in your storage and you can control the lights and the temperature. You can also help to the user to find things in the house. ${username} is the name of the person.`,
+  };
 
   const getGPTResponse = async (prompt: MessageProps[]) => {
     setError(null);
