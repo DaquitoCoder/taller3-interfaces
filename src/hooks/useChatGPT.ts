e5d6a956e7ac4718a6574a3395cb8562312b0672
Alejandro León <juan.aleon2001@gmail.com>
2023-05-29 22:24:55 -0500

    new: created chat gpt hook

--- src/hooks/useChatGPT.ts
+++ src/hooks/useChatGPT.ts
@@ -1,47 +1,52 @@
 import { useEffect, useState } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
+import { MessageProps } from '../components/chat/Message';
 
+type GPTMessage = {
+  role: 'user' | 'assistant' | 'system';
+  content: string;
+};
 
-export function useChatGPT(prompt: string) {
-  const [response, setResponse] = useState(null);
+const firstMessage: GPTMessage = {
+  role: 'system',
+  content:
+    'You are an house automative assistant, your name is HomeGenius. You have to help to the user to control the house. You can search things in your storage and you can control the lights and the temperature. You can also help to the user to find things in the house.',
+};
+
+export function useChatGPT() {
   const [error, setError] = useState(null);
-  const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+  const [loading, setLoading] = useState(false);
+  const apiKey = import.meta.env.VITE_APP_OPENAI_API_KEY;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const configuration = new Configuration({
-          apiKey,
+  const getGPTResponse = async (prompt: MessageProps[]) => {
+    setError(null);
+    setLoading(true);
+    try {
+      const configuration = new Configuration({
+        apiKey,
+      });
+      const messages: GPTMessage[] = [firstMessage];
+      prompt.forEach((message) => {
+        if (message.emissor === 'loading') return;
+        messages.push({
+          role: message.emissor,
+          content: message.messageText,
         });
-        const messages: any = [
-          {
-            role: 'system',
-            content:
-              'You are an house automative assistant. You have to help to the user to control the house. You can search things in your storage and you can control the lights and the temperature. You can also help to the user to find things in the house.',
-          },
-          {
-            role: 'user',
-            content: prompt,
-          },
-        ];
+      });
 
-        const openai = new OpenAIApi(configuration);
+      const openai = new OpenAIApi(configuration);
+      const completion: any = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+      });
 
-        const completion: any = await openai.createChatCompletion({
-          model: 'gpt-3.5-turbo',
-          messages: messages,
-        });
+      return completion.data.choices[0].message.content;
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-        setResponse(completion.data.choices[0].message.content);
-        setError(null);
-      } catch (err: any) {
-        setError(err.message);
-        setResponse(null);
-      }
-    };
-
-    fetchData();
-  }, [prompt]);
-
-  return { response, error };
+  return { error, loading, getGPTResponse };
 }
