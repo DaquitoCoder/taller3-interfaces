a0d5245b190be8d443af4c103ff2707778daf48d
MrWallace34 <113811293+MrWallace34@users.noreply.github.com>
2023-06-01 13:05:20 -0500

    Add files via upload

--- src/components/SpeechRecognition.tsx
+++ src/components/SpeechRecognition.tsx
@@ -129,6 +129,163 @@ const MyComponent = () => {
           asistente.style.display = 'none';
         },
       },
+      {
+        indexes: ['abrir puerta habitación uno'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-hab1');
+          if (puerta) {
+            puerta.style.rotate= '120deg';
+            puerta.style.left = '359px';
+            puerta.style.top = '-365px';
+          }
+        },
+      },
+      {
+        indexes: ['cerrar puerta habitación uno'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-hab1');
+          if (puerta) {
+            puerta.style.rotate= '90deg';
+            puerta.style.left = '355px';
+            puerta.style.top = '-356px';
+          }
+        },
+      },
+      {
+        indexes: ['abrir puerta baño habitación uno'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-bano-habitacion');
+          if (puerta) {
+            puerta.style.rotate='30deg';
+            puerta.style.left='176px';
+            puerta.style.top = '-335px';
+          }
+
+        }
+      },
+      {
+        indexes: ['cerrar puerta baño habitación uno'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-bano-habitacion');
+          if (puerta) {
+            puerta.style.rotate='0deg';
+            puerta.style.left='166px';
+            puerta.style.top = '-338px';
+          }
+
+        }
+      },
+      {
+        indexes: ['abrir puerta habitación dos'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-hab2');
+          if (puerta) {
+            puerta.style.rotate='30deg';
+           
+            puerta.style.left='405px';
+            puerta.style.top = '-368px';
+          }
+        }
+      },
+      {
+        indexes: ['cerrar puerta habitación dos'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-hab2');
+          if (puerta) {
+            puerta.style.rotate='0deg';
+            puerta.style.left='394px';
+            puerta.style.top = '-372px';
+          }
+        }
+      },
+      {
+        indexes: ['abrir puerta habitación tres'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-hab3');
+          if (puerta) {
+            puerta.style.rotate='30deg';
+            puerta.style.left='405px';
+            puerta.style.top = '-331px';
+          }
+        }
+      },
+      {
+        indexes: ['cerrar puerta habitación tres'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-hab3');
+          if (puerta) {
+            puerta.style.rotate='0deg';
+            puerta.style.left='394px';
+            puerta.style.top = '-335px';
+          }
+        }
+      },
+      {
+        indexes: ['abrir puerta baño principal'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-bano-principal');
+          if (puerta) {
+            puerta.style.rotate='30deg';
+            puerta.style.left='313px';
+            puerta.style.top = '-246px';
+          }
+        }
+      },
+      {
+        indexes: ['cerrar puerta baño principal'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-bano-principal');
+          if (puerta) {
+            puerta.style.rotate='0deg';
+            puerta.style.left='323px';
+            puerta.style.top = '-243px';
+          }
+        }
+      },
+      {
+        indexes: ['abrir puerta lavadero'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-lavado');
+          if (puerta) {
+            puerta.style.rotate= '120deg';
+            puerta.style.left = '165px';
+            puerta.style.top = '-370px';
+          }
+        },
+      },
+      {
+        indexes: ['cerrar puerta lavadero'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-lavado');
+          if (puerta) {
+            puerta.style.rotate= '90deg';
+            puerta.style.left = '162px';
+            puerta.style.top = '-359px';
+          }
+        },
+      },
+      {
+        indexes: ['abrir puerta principal'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-principal');
+          if (puerta) {
+            puerta.style.rotate= '120deg';
+            puerta.style.left = '356px';
+            puerta.style.top = '-125px';
+          }
+        },
+      },
+      {
+        indexes: ['cerrar puerta principal'],
+        action: () =>{
+          let puerta = document.getElementById('puerta-principal');
+          if (puerta) {
+            puerta.style.rotate= '90deg';
+            puerta.style.left = '351px';
+            puerta.style.top = '-106px';
+          }
+        },
+      },
     ]);
 
     // Iniciar el reconocimiento de voz
