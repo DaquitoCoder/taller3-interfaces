c9ba0b0479bfd3d965b9e31c4f67668f6a3b990d
Alejandro León <juan.aleon2001@gmail.com>
2023-06-01 22:12:46 -0500

    fix: lights position

--- src/components/Plano.tsx
+++ src/components/Plano.tsx
@@ -13,15 +13,6 @@ const Plano = () => {
 
   return (
     <div className='plano'>
-      <div className='bombillo-cocina' id='bombillo-cocina'></div>
-      <div className='bombillo-comedor' id='bombillo-comedor'></div>
-      <div className='bombillo-sala' id='bombillo-sala'></div>
-      <div className='bombillo-bano-habitacion' id='bombillo-bano-habitacion'></div>
-      <div className='bombillo-lavado' id='bombillo-lavado'></div>
-      <div className='bombillo-bano-principal' id='bombillo-bano-principal'></div>
-      <div className='bombillo-cuarto1' id='bombillo-cuarto1'></div>
-      <div className='bombillo-cuarto2' id='bombillo-cuarto2'></div>
-      <div className='bombillo-cuarto3' id='bombillo-cuarto3'></div>
       <div className='ventana cocina' id='ventana-cocina'></div>
       <div className='ventana sala' id='ventana-sala'></div>
       <div className='ventana comedor' id='ventana-comedor'></div>
@@ -69,6 +60,15 @@ const Plano = () => {
           }}
         ></button>
       </div>
+      <div className='bombillo-cocina' id='bombillo-cocina'></div>
+      <div className='bombillo-comedor' id='bombillo-comedor'></div>
+      <div className='bombillo-sala' id='bombillo-sala'></div>
+      <div className='bombillo-bano-habitacion' id='bombillo-bano-habitacion'></div>
+      <div className='bombillo-lavado' id='bombillo-lavado'></div>
+      <div className='bombillo-bano-principal' id='bombillo-bano-principal'></div>
+      <div className='bombillo-cuarto1' id='bombillo-cuarto1'></div>
+      <div className='bombillo-cuarto2' id='bombillo-cuarto2'></div>
+      <div className='bombillo-cuarto3' id='bombillo-cuarto3'></div>
     </div>
   );
 };
